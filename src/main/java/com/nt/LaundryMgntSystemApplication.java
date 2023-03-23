89e4774ff06ccac01c26ea3f0f941037fe1eeae6
Soumya <soumyaranjanpanda.199@gmail.com>
2023-03-23 17:58:58 +0530

    int i declared

--- src/main/java/com/nt/LaundryMgntSystemApplication.java
+++ src/main/java/com/nt/LaundryMgntSystemApplication.java
@@ -8,8 +8,6 @@ public class LaundryMgntSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaundryMgntSystemApplication.class, args);
-		System.out.println("Hello World Task-2");
-		System.out.println("Hello World Task-1 new");
 	}
 
 }
